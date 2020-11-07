6251e927ccff601de7562c8e2420b2957554708b
JohnLindblad <johnlindblad1996@gmail.com>
2020-11-07 14:09:56 +0100

    Adding clustering and started plotting

--- finalProject.py
+++ finalProject.py
@@ -17,16 +17,23 @@ import matplotlib.pyplot as plt
 # scientific and more advanced packages
 import scipy.signal as signal
 import ruptures as rpt
+from sklearn.cluster import KMeans
 
 # set path and id:s for Liverpool games
-path = '/mnt/d/GitHub/opendata/data' #set the path to the data-folder in the repo
+# path = '/mnt/d/GitHub/opendata/data' #set the path to the data-folder in the repo
+path = 'D:\\GitHub\\opendata\\data'
+linux_path = False
 lfc_matches = ['4039', '2440'] # manually identified the match_id for the Liverpool games
 lfc_id = 2 # manually identified the id for Liverpool
 
 # create a list of all match_ids
-def create_match_list(path):
-    with open(path + '/matches.json') as f:
-        data = json.load(f)
+def create_match_list(path, linux_path = True):
+    if linux_path:
+        with open(path + '/matches.json') as f:
+            data = json.load(f)
+    else:
+        with open(path + '\\matches.json') as f:
+            data = json.load(f)
 
     match_ids = []
 
@@ -35,10 +42,10 @@ def create_match_list(path):
 
     return match_ids
 
-all_matches = create_match_list(path)
+all_matches = create_match_list(path, linux_path)
 
 # create player_dict for Liverpool players
-def create_player_dicts(path, match_ids, team_id):
+def create_player_dicts(path, match_ids, team_id, linux_path=True):
     lfc_id = team_id
 
     lfc_dict = {}
@@ -47,9 +54,14 @@ def create_player_dicts(path, match_ids, team_id):
     all_players_list = []
 
     for match in match_ids:
-
-        with open(path + '/matches/' + match + '/match_data.json') as f:
-            data = json.load(f)
+        
+        if linux_path:
+            with open(path + '/matches/' + match + '/match_data.json') as f:
+                data = json.load(f)
+        else:
+            with open(path + '\\matches\\' + match + '\\match_data.json') as f:
+                data = json.load(f)
+    
 
         for player in data['players']:
             obj_id = player['trackable_object']
@@ -77,20 +89,24 @@ def create_player_dicts(path, match_ids, team_id):
 
     return lfc_dict, lfc_list, name_dict, all_players_list
 
-lfc_dict, lfc_list, name_dict, all_players_list = create_player_dicts(path, match_ids = all_matches, team_id = lfc_id)
+lfc_dict, lfc_list, name_dict, all_players_list = create_player_dicts(path, match_ids = all_matches, team_id = lfc_id, linux_path=linux_path)
 
 # load the tracking data
-def load_tracking_data(path, match_ids, drop_limit=6000):
+def load_tracking_data(path, match_ids, drop_limit=6000, linux_path=True):
 
     match_dfs_list = []
 
     # tot_df = pd.DataFrame()
 
     for match in match_ids:
-
-        with open(path + '/matches/' + match + '/structured_data.json') as f:
-            data = json.load(f)
-
+        
+        if linux_path:
+            with open(path + '/matches/' + match + '/structured_data.json') as f:
+                data = json.load(f)
+        else:
+            with open(path + '\\matches\\' + match + '\\structured_data.json') as f:
+                data = json.load(f)
+        
         frame_list =  []
 
         for frame in data:
@@ -215,7 +231,11 @@ def summary_stats(vel_acc_df, player_match_list, acc_threshold=2, dec_threshold=
 
     return stats_df
 
-stats = summary_stats(vel_acc_df_all, pm_list, acc_threshold=2, dec_threshold=-2)
+try:
+    stats = pd.read_pickle('summary_stats.pkl')
+except:
+    stats = summary_stats(vel_acc_df_all, pm_list, acc_threshold=2, dec_threshold=-2)
+    stats.to_pickle('summary_stats.pkl')
 
 
 def summary_stats_aggregated(summary_stats, players_list):
@@ -259,14 +279,53 @@ def summary_stats_aggregated(summary_stats, players_list):
 
     return stats_aggregated_df
 
-stats_aggregated = summary_stats_aggregated(stats, all_players_list)
-print(stats_aggregated)
+try:
+    stats_aggregated = pd.read_pickle('stats_aggregated.pkl')
+except:
+    stats_aggregated = summary_stats_aggregated(stats, all_players_list)
+    stats_aggregated.to_pickle('stats_aggregated.pkl')
 
-'''TO-DO:
-1) apply some clustering
-2) produce plots
-    - dot plot with acc/dec ratio
-    - plot the points and the result from the clustering model (hard if using more than 3 or even 2 variabless)
-'''
+def filtered_df(df, player_list):
+    row_list = []
+    
+    for player in player_list:
+        if player in df.index:
+            row = df.loc[player]
+            row_list.append(row)
+            
+    lfc_df = pd.DataFrame(row_list)
+    return lfc_df
+    
+lfc_df = filtered_df(stats_aggregated, lfc_list)
+
+def transform_to_points(df, col1, col2):
+    point_list = list(zip(df[col1], df[col2]))
+    x_list = list(df[col1])
+    y_list = list(df[col2])
+
+    return point_list, x_list, y_list
+
+point_list, x_list, y_list = transform_to_points(stats_aggregated, 'distance_covered', 'acc/dec-ratio')
+
+def kmeans_profiling(points, k):
+    kmeans = KMeans(n_clusters = k, random_state=0).fit(points)
+    cluster_labels = kmeans.labels_
+
+    return cluster_labels
+
+cluster_labels = kmeans_profiling(point_list, 3)
+
+# plot the cluster
+fig = plt.figure()
+plt.scatter(x_list, y_list, c = cluster_labels)
+fig.savefig('clusterplot.png', dpi=100)
+
+# histogram acc/dec ratio
+fig, axs = plt.subplots(nrows=2, ncols=1)
+axs[0].hist(stats_aggregated['acc/dec-ratio'])
+axs[0].axis([0.8, 1.2, 0, 40])
+axs[1].hist(lfc_df['acc/dec-ratio'], color='r')
+axs[1].axis([0.8, 1.2, 0, 5])
+fig.savefig('adr_hist.png', dpi=100)
 
 print(f'Time to run the script = {time.time()- t0}')
